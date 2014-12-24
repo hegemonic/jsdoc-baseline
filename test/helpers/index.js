6efe6260bf9f5e0206e48ff0f9287f0d319b3814
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-12-23 19:30:02 -0800

    remove unused code and inaccurate comment

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -3,31 +3,8 @@
 'use strict';
 
 var path = require('path');
-var temp = require('temp').track();
-
-var tempPath = null;
-
-var TEMP_DIRECTORY = exports.TEMP_DIRECTORY = 'baseline-test';
-
-// Creates a temp directory relative to the main temp directory.
-exports.createTempDirectory = function createTempDirectory(dirPath) {
-    var resolvedPath;
-
-    // create the main temp directory if necessary
-    if (!tempPath) {
-        resolvedPath = tempPath = temp.mkdirSync(TEMP_DIRECTORY);
-    }
-
-    if (dirPath) {
-        resolvedPath = path.resolve(TEMP_DIRECTORY, dirPath);
-        temp.mkdirSync(dirPath);
-    }
-
-    return resolvedPath;
-};
 
 // Resets global variables used by JSDoc to the default values for tests.
-// TODO: this is also used in the gulpfile, so find another place for it
 exports.resetJsdocGlobals = function resetJsdocGlobals() {
     global.env = {
         conf: {
