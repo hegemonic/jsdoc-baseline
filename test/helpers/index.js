a812215e2ee0a00ffe2785dbc5550924015c7041
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-12-23 21:07:11 -0800

    add test helpers for rendering a view and extending the built-in assertions

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -4,8 +4,40 @@
 
 var path = require('path');
 
+var template;
+
+// Adds matcher functions to the global `expect` instance. Call this method at the start of any
+// test spec that uses the custom matchers.
+exports.addMatchers = function() {
+    var expect = global.expect;
+
+    expect.addAssertion('toContainString', function(value) {
+        var message = this.generateMessage(this.value, this.expr, 'to contain the string ' + value);
+
+        if (this.value.indexOf(value) !== -1) {
+            return this.assertions.pass(message);
+        }
+
+        this.assertions.fail(message);
+    });
+};
+
+// Provides a template-rendering function that can be shared across modules.
+exports.render = function() {
+    var Template;
+
+    exports.setup();
+
+    if (!template) {
+        Template = require('../../lib/template');
+        template = new Template('.').init();
+    }
+
+    return template.render.apply(template, arguments);
+};
+
 // Resets global variables used by JSDoc to the default values for tests.
-exports.resetJsdocGlobals = function resetJsdocGlobals() {
+exports.resetJsdocGlobals = function() {
     global.env = {
         conf: {
             templates: {
