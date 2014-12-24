7c5918a6516890351fa5f6283cbcee9f19137401
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-12-23 21:07:18 -0800

    tests for copyright atom

---
+++ test/specs/views/atoms/copyright.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var helpers = require('../../../helpers');
+
+helpers.addMatchers();
+
+describe('copyright atom', function() {
+    it('should generate nothing if there is no copyright owner', function() {
+        var text = helpers.render('copyright', {});
+
+        expect(text).toBe('');
+    });
+
+    it('should show the copyright owner', function() {
+        var text = helpers.render('copyright', {
+            copyright: 'John Doe'
+        });
+
+        expect(text).toContainString('<dd>John Doe</dd>');
+    });
+
+    it('should link to the copyright owner when appropriate', function() {
+        var text = helpers.render('copyright', {
+            copyright: 'http://example.org'
+        });
+
+        expect(text).toContainString('<a href="http://example.org">http://example.org</a>');
+    });
+});
