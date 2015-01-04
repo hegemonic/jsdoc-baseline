47fd3ff4166c830ed31eff73bfef6e60e8f1e489
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-04 09:21:28 -0800

    refinements to filefinder module (#117)

--- test/specs/lib/filefinder.js
+++ test/specs/lib/filefinder.js
@@ -2,6 +2,7 @@
 
 describe('lib/filefinder', function() {
     var fileFinder = require('../../../lib/filefinder');
+    var finder;
 
     var fixturePaths = [
         'test/fixtures/filefinder/pathA',
@@ -9,12 +10,15 @@ describe('lib/filefinder', function() {
     ];
 
     beforeEach(function() {
-        fileFinder = new fileFinder.FileFinder();
-        fileFinder.loadPathsSync(fixturePaths);
+        finder = fileFinder.get('default', fixturePaths, {
+            forceUpdate: true
+        });
     });
 
     afterEach(function() {
-        fileFinder = new fileFinder.FileFinder();
+        finder = fileFinder.get('default', [], {
+            forceUpdate: true
+        });
     });
 
     it('should be an object', function() {
@@ -23,25 +27,23 @@ describe('lib/filefinder', function() {
 
     it('should export its constructor', function() {
         expect(fileFinder.FileFinder).toBeFunction();
-        expect(fileFinder).toBeInstanceOf(fileFinder.FileFinder);
     });
 
-    it('should export a "loadPathsSync" method', function() {
-        expect(fileFinder.loadPathsSync).toBeFunction();
+    it('should export a "get" method', function() {
+        expect(fileFinder.get).toBeFunction();
     });
 
-    it('should export a "readFileSync" method', function() {
-        expect(fileFinder.readFileSync).toBeFunction();
-    });
+    describe('get', function() {
+        it('should return the current default finder if no arguments are provided', function() {
+            var defaultFinder = fileFinder.get('default');
+            var noArgsFinder = fileFinder.get();
 
-    it('should export a "require" method', function() {
-        expect(fileFinder.require).toBeFunction();
-    });
+            expect(noArgsFinder).toBe(defaultFinder);
+        });
 
-    describe('loadPathsSync', function() {
         it('should accept an array of valid paths', function() {
             function load() {
-                fileFinder.loadPathsSync(fixturePaths);
+                fileFinder.get('default', fixturePaths);
             }
 
             expect(load).not.toThrow();
@@ -49,67 +51,101 @@ describe('lib/filefinder', function() {
 
         it('should throw an error if a path is invalid', function() {
             function load() {
-                fileFinder.loadPathsSync(['/no/such/path/at/all']);
+                fileFinder.get('default', ['/no/such/path/at/all']);
             }
 
             expect(load).toThrow();
         });
+
+        it('should not recreate the finder if the same array is passed in', function() {
+            var fileArray = [];
+            var defaultFinder = fileFinder.get('default', fileArray);
+            var currentFinder = fileFinder.get('default', fileArray);
+
+            expect(currentFinder).toBe(defaultFinder);
+        });
+
+        it('should not recreate the finder if two functionally identical arrays are passed in',
+            function() {
+            var arrayA = [fixturePaths[0]];
+            var arrayB = [fixturePaths[0]];
+            var defaultFinder = fileFinder.get('default', arrayA);
+            var currentFinder = fileFinder.get('default', arrayB);
+
+            expect(currentFinder).toBe(defaultFinder);
+        });
+
+        it('should recreate the finder if an array of a different length is provided', function() {
+            var defaultFinder = fileFinder.get('default', [fixturePaths[0]]);
+            var currentFinder = fileFinder.get('default', fixturePaths);
+
+            expect(currentFinder).not.toBe(defaultFinder);
+        });
+
+        it('should recreate the finder if an array of the same length, with different contents, ' +
+            'is passed in', function() {
+            var defaultFinder = fileFinder.get('default', fixturePaths);
+            var currentFinder = fileFinder.get('default', fixturePaths.slice(0).reverse());
+
+            expect(currentFinder).not.toBe(defaultFinder);
+        });
     });
 
-    describe('readFileSync', function() {
-        it('should return the first instance of the file that it finds', function() {
-            function read() {
-                return fileFinder.readFileSync('asset1.txt', 'utf8');
-            }
+    describe('FileFinder', function() {
+        describe('readFileSync', function() {
+            it('should return the first instance of the file that it finds', function() {
+                function read() {
+                    return finder.readFileSync('asset1.txt', 'utf8');
+                }
 
-            expect(read).not.toThrow();
-            expect(read()).toContain('Asset 1, path A');
+                expect(read).not.toThrow();
+                expect(read()).toContain('Asset 1, path A');
+            });
+
+            it('should find files that do not exist in the first directory', function() {
+                function read() {
+                    return finder.readFileSync('asset2.txt', 'utf8');
+                }
+
+                expect(read).not.toThrow();
+                expect(read()).toContain('Asset 2, path B');
+            });
+
+            it('should throw an error if the file does not exist', function() {
+                function read() {
+                    return finder.readFileSync('fluffy.txt', 'utf8');
+                }
+
+                expect(read).toThrow();
+            });
         });
 
-        it('should find files that do not exist in the first directory', function() {
-            function read() {
-                return fileFinder.readFileSync('asset2.txt', 'utf8');
-            }
+        describe('require', function() {
+            it('should return the first instance of the module that it finds', function() {
+                function req() {
+                    return finder.require('./module1');
+                }
 
-            expect(read).not.toThrow();
-            expect(read()).toContain('Asset 2, path B');
-        });
+                expect(req).not.toThrow();
+                expect(req()).toBe('Module 1, path A');
+            });
 
-        it('should throw an error if the file does not exist', function() {
-            function read() {
-                return fileFinder.readFileSync('fluffy.txt', 'utf8');
-            }
+            it('should find modules that do not exist in the first directory', function() {
+                function req() {
+                    return finder.require('./module2');
+                }
 
-            expect(read).toThrow();
-        });
-    });
+                expect(req).not.toThrow();
+                expect(req()).toBe('Module 2, path B');
+            });
 
-    describe('require', function() {
-        it('should return the first instance of the module that it finds', function() {
-            function req() {
-                return fileFinder.require('module1');
-            }
+            it('should throw an error if the module does not exist', function() {
+                function req() {
+                    return finder.require('./fluffy');
+                }
 
-            req();
-            expect(req).not.toThrow();
-            expect(req()).toBe('Module 1, path A');
-        });
-
-        it('should find modules that do not exist in the first directory', function() {
-            function req() {
-                return fileFinder.require('module2');
-            }
-
-            expect(req).not.toThrow();
-            expect(req()).toBe('Module 2, path B');
-        });
-
-        it('should throw an error if the module does not exist', function() {
-            function req() {
-                return fileFinder.require('fluffy');
-            }
-
-            expect(req).toThrow();
+                expect(req).toThrow();
+            });
         });
     });
 });
